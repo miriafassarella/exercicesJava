3e926f7b5fd726f9348526b28a909c1b0d860501
Miriã Fassarella <miriafassarella@gmail.com>
2025-05-17 21:33:53 -0400

    aula 2.5

--- OlaMergulhador.java
+++ OlaMergulhador.java
@@ -1,7 +1,11 @@
+//voce pode comentar uma linha inteira
 public class OlaMergulhador{
 	public static void main(String[] args){
+		//imprime a mensagem na saída padrão
 		System.out.println("Olá, merglhador!");
 
 }
+/*Por convenção adcionamos um espaço entre o nome da classe e a abertura de 
+chaves, tambem a identação é feita com quatro espaços*/
 
 }
