8c08d4a6c40e0a124054fda9834f7b070c7cbe37
Miriã Fassarella <miriafassarella@gmail.com>
2025-05-18 22:41:01 -0400

    Aula 2.7 - variáveis

---
+++ AulaVariaveis.java
@@ -0,0 +1,15 @@
+public class AulaVariaveis {
+    public static void main(String[] args) {
+	//Não recomendado pela Code Conventions da Oracle
+	String miria = "41", Adriano = "42";
+    
+	//lowerCamelCase
+        int minhaIdade;
+	minhaIdade = 40;
+	int suaIdade = 25;
+	int totalIdades = minhaIdade + suaIdade;
+	
+	System.out.println("Total das idades: " + totalIdades);
+    }
+
+}
