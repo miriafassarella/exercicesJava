c2c60fe339dad70e3678c8116bba1893d3ef846a
Miriã Fassarella <miriafassarella@gmail.com>
2025-05-20 12:18:13 -0400

    Desafio - variáveis e operadores

---
+++ DesafioVariaveisEOperadores.java
@@ -0,0 +1,14 @@
+public class DesafioVariaveisEOperadores {
+	
+    public static void main(String[] args) {
+	
+	int venda1 = 20;
+	int venda2 = 30;
+	int venda3 = 100;
+	int mediaDasUltimasTresVendas = (venda1 + venda2 + venda3) / 3;
+
+	System.out.println("A média das ultimas três vendas foi: " + mediaDasUltimasTresVendas);
+    }
+
+
+}
