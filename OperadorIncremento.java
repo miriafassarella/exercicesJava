5c3b8aed1613989cf8dbbeea925aee5b3c4e2c6a
Miriã Fassarella <miriafassarella@gmail.com>
2025-05-21 12:24:46 -0400

    aula 2.11

---
+++ OperadorIncremento.java
@@ -0,0 +1,22 @@
+public class OperadorIncremento {
+
+    public static void main(String[] args) {
+    
+        int totalDownloads = 10;
+	
+	//Aqui ele atribui primeiro para depois incrementar.
+	//int novoTotalDownloads = totalDownloads++;
+
+	//Aqui ele incrementa primeiro para depois atribuir.
+	int novoTotalDownloads = ++totalDownloads;
+	
+	int decremento = 10;
+	//Aqui ele dcrementa para depois atribuir.
+	int novoValorDecremento = --decremento;
+	
+	System.out.println("Total de downloads: " + novoTotalDownloads);
+	System.out.println("Valor decremento: " + novoValorDecremento);
+
+    }
+
+}
