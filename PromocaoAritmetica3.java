f899040b61940e456f29dba0eb5e1a9c9ab83eb0
Miriã Fassarella <miriafassarella@gmail.com>
2025-05-28 12:26:08 -0400

    aula 2.17

---
+++ PromocaoAritmetica3.java
@@ -0,0 +1,14 @@
+public class PromocaoAritmetica3 {
+    public static void main(String[] args) {
+	
+	int x = 3;
+	int y = 2;
+	float z = (float) x / y; 
+	
+
+	System.out.println(z);
+
+    }
+
+
+}
