79d5cfafa40a33273d0b9efacedea2173aca54f2
Miriã Fassarella <miriafassarella@gmail.com>
2025-07-01 08:17:44 -0400

    variável como referencia

---
+++ aula-oo/TestReferencias.java
@@ -0,0 +1,24 @@
+public class TestReferencias {
+
+    public static void main(String[] args) {
+
+	Pessoa pessoa1 = new Pessoa();
+	pessoa1.nome = "Miria";
+	pessoa1.cpf = "104.332.117-92";
+	pessoa1.anoNascimento = 1984;
+
+
+	Carro meuCarro = new Carro(); //meuCarro é apenas o nome da variável que faz referencia ao objeto.
+	meuCarro.proprietario = pessoa1;
+
+	Carro seuCarro = new Carro();
+	seuCarro.proprietario = pessoa1;
+	seuCarro.proprietario.nome = "João";
+
+
+	System.out.printf("Proprietario do meu carro: %s%n", meuCarro.proprietario.nome);
+	System.out.printf("Proprietario do seu carro: %s%n",seuCarro.proprietario.nome);
+
+
+    }
+}
