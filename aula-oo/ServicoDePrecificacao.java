f71c4dfc3474ae6f47d11b39546dbcd217f5422f
Miriã Fassarella <miriafassarella@gmail.com>
2025-07-15 12:38:00 -0400

    aula 5.28 - metodos nao alteram valores primitivos

---
+++ aula-oo/ServicoDePrecificacao.java
@@ -0,0 +1,11 @@
+public class ServicoDePrecificacao {
+
+    public double calcularprecoVenda(double precoCusto) {
+
+	 return precoCusto * 1.20;;
+
+	//evitar criar metodos que modificam valores de variavel que voce recebeu como parametro.
+    }
+
+
+}
