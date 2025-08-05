ef555c9409da306ee55975cabda31a5392b93be4
Miriã Fassarella <miriafassarella@gmail.com>
2025-08-05 10:10:23 -0400

    aula 8.3

---
+++ aula-oo/Array.java
@@ -0,0 +1,26 @@
+public class Array{
+
+    public static void main(String[] args) {
+
+	int[] notas = {8, 5, 4, 9, 18}; // array é um objeto
+
+	notas[3] = 20; 
+	double soma = 0;
+
+	//System.out.print(notas[notas.length - 2]);
+
+
+	//usado para quando você não pecisa do indice.
+	//int j = 0;
+	for (int i : notas) {
+
+	    soma += i;
+	   // j++;
+	}
+
+	System.out.print("Média de notas: " + (soma / notas.length));
+    }
+
+    
+
+}
