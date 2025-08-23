490f07e36f44bf381d3cf2645038af9e1fccc0a3
Miriã Fassarella <miriafassarella@gmail.com>
2025-08-23 10:29:42 -0400

    Construtores

---
+++ Teste.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+
+public class Teste {
+
+    public static void main(String[] args) {
+
+	Scanner entrada = new Scanner(System.in);
+
+	System.out.print("Diga seu nome: ");
+	String nome = entrada.nextLine();
+
+	System.out.printf("Olá, %s", nome);
+
+
+     }
+
+
+}
---
+++ aula-oo/construtores/Produto.java
@@ -0,0 +1,14 @@
+public class Produto {
+
+     Produto() {
+	System.out.println("Eu sou um contrutor da classe Produto");
+    }
+
+	//Quando voce quer garantir um estado inicial dos objetos instanciados de uma classe.
+
+	//neste caso ele seria um método e não um construtor
+     /*void Produto() {
+	System.out.println("Eu sou um contrutor da classe Produto");
+    }*/
+
+}
