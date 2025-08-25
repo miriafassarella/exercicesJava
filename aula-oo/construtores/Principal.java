63d02e85447ea2d47a8324e4f28e9d631540ca61
Miriã Fassarella <miriafassarella@gmail.com>
2025-08-25 12:14:35 -0400

    Sbrecarga de construtores

--- aula-oo/construtores/Principal.java
+++ aula-oo/construtores/Principal.java
@@ -5,6 +5,25 @@ public class Principal {
 	Produto produto1 = new Produto("Picanha");
 	Produto produto2 = new Produto("Alcatra");
 
+	Produto produto3 = new Produto("Fraudinha", 35);
+	Produto produto4 = new Produto();
+
+	
+
+	System.out.printf("Produto1 (nome) : %s | Produto1 (quantidade) : %d", produto1.nome, produto1.quantidadeEstoque);
+	System.out.printf("Produto2 (nome) : %s | Produto2 (quantidade) : %d", produto2.nome, produto2.quantidadeEstoque);
+
+	System.out.printf("Produto3 (nome) : %s | Produto3 (quantidade) : %d", produto3.nome, produto3.quantidadeEstoque);
+
+	System.out.printf("Produto4 (nome) : %s | Produto4 (quantidade) : %d", produto4.nome, produto4.quantidadeEstoque);
+
+
+
     }
+
+	
+	
+
+	
 
 }
--- aula-oo/construtores/Produto.java
+++ aula-oo/construtores/Produto.java
@@ -5,6 +5,19 @@ public class Produto {
      String nome;
      int quantidadeEstoque;
 
+     Produto() {
+
+	this.nome = "Sem nome";
+	this.quantidadeEstoque = QUANTIDADE_ESTOQUE_INICIAL;
+
+     }
+
+     Produto(String nome, int quantidadeEstoque) {
+	this.nome = nome;
+	this.quantidadeEstoque = quantidadeEstoque;
+
+     }
+
      Produto(String nome) {
 	System.out.println("Eu sou um contrutor da classe Produto");
 
