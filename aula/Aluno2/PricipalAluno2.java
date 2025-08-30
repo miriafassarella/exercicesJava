3bcd8983fdb321ed2406766989ab6a0fdaca8675
Miriã Fassarella <miriafassarella@gmail.com>
2025-08-29 20:37:30 -0400

    modificadores de acesso public e default

---
+++ aula/Aluno2/PricipalAluno2.java
@@ -0,0 +1,16 @@
+
+
+import aluno.Aluno;
+
+public class PricipalAluno2 {
+    public static void main(String[] args) {
+
+	Aluno aluno1 = new Aluno();
+
+	aluno1.nome = "Miriã";
+	aluno1.idade = 41;
+
+	System.out.print(aluno1.nome);
+
+    }
+}
---
+++ aula/Aluno2/aluno/Aluno.java
@@ -0,0 +1,6 @@
+package aluno;
+
+public class Aluno {
+   public String nome;
+   public int idade;
+}
