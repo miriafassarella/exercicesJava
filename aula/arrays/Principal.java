5e218e4b8ff484380df37ef7131e70a6e34a56e2
Miriã Fassarella <miriafassarella@gmail.com>
2025-09-07 17:21:46 -0400

    System.out.print(Arrays.toString(nom array) : transformant un array en String

---
+++ aula/arrays/Principal.java
@@ -0,0 +1,14 @@
+import java.util.Arrays;
+
+public class Principal {
+
+    public static void main(String[] args) {
+	int[] notas = {8, 5, 4, 9};
+
+	
+
+	System.out.print(Arrays.toString(notas));
+
+    }
+
+}
