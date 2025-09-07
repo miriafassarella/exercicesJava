f8d9cdaf5d604490bc962d977b316f290b0d04d7
Miriã Fassarella <miriafassarella@gmail.com>
2025-09-07 17:35:30 -0400

    Arrays.sort(nom array, Comparator.reverseOrder()) : pour mettre en ordre inverse, quand nous avons un array d'objet

--- aula/arrays/Principal.java
+++ aula/arrays/Principal.java
@@ -1,13 +1,15 @@
 import java.util.Arrays;
+import java.util.Comparator;
 
 public class Principal {
 
     public static void main(String[] args) {
-	int[] notas = {8, 5, 4, 9};
+	Integer[] notas = {8, 5, 4, 9};
 
-	Arrays.sort(notas); // modifie le array en le mettant en ordre
+	Arrays.sort(notas, Comparator.reverseOrder()); // modifie le array en le mettant en ordre inverse.
 
 
+	//ne modifie pas le array
 	System.out.print(Arrays.toString(notas));
 
 	
