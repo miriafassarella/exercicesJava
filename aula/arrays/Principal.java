f07578c1fcbe447fd2b1ca34f34118fe78739e1e
Miriã Fassarella <miriafassarella@gmail.com>
2025-09-07 17:28:04 -0400

    Arrays.sort(nom array) : modifie en le mettant en ordre

--- aula/arrays/Principal.java
+++ aula/arrays/Principal.java
@@ -5,9 +5,12 @@ public class Principal {
     public static void main(String[] args) {
 	int[] notas = {8, 5, 4, 9};
 
-	
+	Arrays.sort(notas); // modifie le array en le mettant en ordre
+
 
 	System.out.print(Arrays.toString(notas));
+
+	
 
     }
 
