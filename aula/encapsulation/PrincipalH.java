07316bb2d9e47f2f08a889edebb2ecbaf3235070
Miria Fassarela <miriafassarella@gmail.com>
2025-10-01 12:51:51 -0400

    Tell dont ask

---
+++ aula/encapsulation/PrincipalH.java
@@ -0,0 +1,12 @@
+public class PrincipalH {
+
+//Tell Don't Ask
+
+    public static void main(String[] args) {
+	Horario horario = new Horario(10, 55);
+
+	System.out.println(horario.formatar());
+
+    }
+
+}
