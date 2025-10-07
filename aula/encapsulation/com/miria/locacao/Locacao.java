0007fd41422d5959ac2182f4202fbab738a636a9
Miria Fassarela <miriafassarella@gmail.com>
2025-10-07 07:42:08 -0400

    continuacao aula 11.7

--- aula/encapsulation/com/miria/locacao/Locacao.java
+++ aula/encapsulation/com/miria/locacao/Locacao.java
@@ -1,5 +1,37 @@
 public class Locacao {
 
+    private Veiculo veiculo;
+    private int quantidadeDiarias;
 
 
+    public Locacao() {
+    }
+
+    public Locacao(Veiculo veiculo, int quantidadeDiarias) {
+	this.veiculo = veiculo;
+	this.quantidadeDiarias = quantidadeDiarias;
+
+    }
+
+
+    public Veiculo getVeiculo() {
+	return veiculo;
+
+    }
+
+    public void setVeiculo(Veiculo veiculo) {
+	this.veiculo = veiculo;
+
+    }
+
+
+    public int getQuantidadeDiarias() {
+	return quantidadeDiarias;
+
+    }
+
+    public void setQuantidadeDiarias(int quantidadeDiarias) {
+	this.quantidadeDiarias = quantidadeDiarias;
+
+    }
 }
--- aula/encapsulation/com/miria/locacao/Veiculo.java
+++ aula/encapsulation/com/miria/locacao/Veiculo.java
@@ -1,5 +1,41 @@
 public class Veiculo {
 
+    private String placa;
+    private boolean disponivel = true;
+    private GrupoVeiculo grupo;
 
+    public Veiculo() {
+
+    }
+
+    public Veiculo(String placa, GrupoVeiculo grupo) {
+	this.placa = placa;
+	this.grupo = grupo;
+
+    }
+
+    public String getPlaca() {
+	return placa;
+    }
+
+    public void getPlaca(String placa) {
+	this.placa = placa;
+    }
+
+    public boolean isDisponivel() {
+	return disponivel;
+    }
+
+    public void setDisponivel(boolean disponivel) {
+	this.disponivel = disponivel;
+    }
+
+    public GrupoVeiculo getGrupoVeiculo() {
+	return grupo;
+    }
+
+    public void setGrupoVeiculo() {
+	this.grupo = grupo;
+    }
 
 }
