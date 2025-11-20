e0e98263da754313408dc9d646e3f02fe53dd662
Miria Fassarela <miriafassarella@gmail.com>
2025-11-20 15:09:24 -0500

    aula 11.8

---
+++ StaticClass.java
@@ -0,0 +1,10 @@
+public class StaticClass {
+
+	private StaticClass(){}
+
+	public static void soma(int number) {
+
+		System.out.printf("Number * 3 = %d%n", number * 3);
+	}
+
+}
---
+++ StaticPrincipal.java
@@ -0,0 +1,10 @@
+public class StaticPrincipal {
+
+    public static void main(String[] args){
+	/*StaticClass newInstance = new StaticClass();*/
+
+	StaticClass.soma(3);
+
+    }
+
+}
