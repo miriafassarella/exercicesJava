376bf2223dbecce9e3afd9f0ed57c8eaa674e96e
Miria Fassarela <miriafassarella@gmail.com>
2025-11-24 12:47:55 -0500

    aula 11.9 part-1

---
+++ com.agenda/src/Horario.java
@@ -0,0 +1,39 @@
+public class Horario {
+
+    private int hora;
+    private int minuto;
+
+    public Horario(int hora, int minuto){
+        setHora(hora);
+        setMinuto(minuto);
+
+    }
+
+    public int getHora() {
+        return hora;
+    }
+
+    public void setHora(int hora) {
+
+        if (hora < 0 || hora > 23) {
+            throw new IllegalArgumentException("Hora invalida: " + hora);
+        }
+        this.hora = hora;
+    }
+
+    public int getMinuto() {
+        return minuto;
+    }
+
+    public void setMinuto(int minuto) {
+
+        if (minuto > 59 || minuto < 0) {
+
+            throw new IllegalArgumentException("Minuto invalido: " + minuto);
+        }
+        this.minuto = minuto;
+    }
+
+
+
+}
